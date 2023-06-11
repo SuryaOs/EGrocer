85f9af58770be9d732d2007bf77d102f8b11e883
Surya <ossurya97@gmail.com>
2023-06-11 12:50:03 +0530

    Angular UI Setup with Routing

---
+++ EGrocer.UI/EGrocer/src/app/features/categories/details/details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class CategoryDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ EGrocer.UI/EGrocer/src/app/features/products/summary/summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
