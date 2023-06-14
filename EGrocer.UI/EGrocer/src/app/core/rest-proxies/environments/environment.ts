c4198c550291a4418445dbdd58b06e557fd4b8da
Surya <ossurya97@gmail.com>
2023-06-14 21:02:24 +0530

    Implemented File Upload logic in frontend

--- EGrocer.UI/EGrocer/src/app/core/rest-proxies/environments/environment.ts
+++ EGrocer.UI/EGrocer/src/app/core/rest-proxies/environments/environment.ts
@@ -2,8 +2,11 @@ import { CategoryServiceToken } from "src/app/features/categories/service/catego
 import { CategoryMockService } from "src/app/features/categories/service/category.mock.service";
 import { ProductServiceToken } from "src/app/features/products/service/product-i.service";
 import { ProductMockService } from "src/app/features/products/service/product.mock.service";
+import { FileUploadServiceToken } from "src/app/shared/service/file-upload-i.service";
+import { FileUploadMockService } from "src/app/shared/service/file-upload.mock.service";
 
 export const restServices = [
   { provide: ProductServiceToken, useClass: ProductMockService},
-  { provide: CategoryServiceToken, useClass: CategoryMockService}
+  { provide: CategoryServiceToken, useClass: CategoryMockService},
+  { provide: FileUploadServiceToken, useClass: FileUploadMockService}
 ]
