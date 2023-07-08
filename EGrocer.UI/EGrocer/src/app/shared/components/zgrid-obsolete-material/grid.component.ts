169f97e794b138e3b5dfbc146ad5bf0c213b0a3a
Surya <ossurya97@gmail.com>
2023-07-08 14:12:02 +0530

    made material component obsolete

---
+++ EGrocer.UI/EGrocer/src/app/shared/components/zgrid-obsolete-material/grid.component.ts
@@ -0,0 +1,25 @@
+// import { Component, Input } from '@angular/core';
+// import { IGridColumn } from '../../interfaces/grid-column-i';
+
+// @Component({
+//   selector: 'app-grid-material',
+//   templateUrl: './grid.component.html',
+//   styleUrls: ['./grid.component.scss']
+// })
+// export class MaterialGridComponent {
+//   @Input() dataSource!: any[];
+//   @Input() columns!: IGridColumn[];
+
+//   // Get the displayed columns from the input columns
+//   get displayedColumns(): string[] {
+//     return [...this.columns.map(column => column.propertyName), 'actions']; // Include 'actions' column
+//   }
+
+//   editItem(item: any){
+//     console.log(item);
+//   }
+
+//   deleteItem(item: any){
+//     console.log(item);
+//   }
+// }
--- EGrocer.UI/EGrocer/src/app/shared/shared.module.ts
+++ EGrocer.UI/EGrocer/src/app/shared/shared.module.ts
@@ -3,17 +3,13 @@ import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { CartComponent } from "./components/cart/cart.component";
-import { GridComponent } from './components/grid/grid.component';
 
-import { MatTableModule } from '@angular/material/table';
-import { MatIconModule } from '@angular/material/icon';
 
-const components = [HeaderComponent, FooterComponent, CartComponent, GridComponent];
-const materialModule = [MatTableModule, MatIconModule];
+const components = [HeaderComponent, FooterComponent, CartComponent];
 
 @NgModule({
   declarations: [components],
-  imports: [CommonModule, materialModule],
+  imports: [CommonModule],
   exports: [components],
 })
 export class SharedModule {}
