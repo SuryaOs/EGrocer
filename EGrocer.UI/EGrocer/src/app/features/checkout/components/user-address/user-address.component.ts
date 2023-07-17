611e53748935ccb91ec89b75e843cedd3a92079c
Surya <ossurya97@gmail.com>
2023-07-17 21:55:05 +0530

    Impemented Address Change Functionality with HTML and SCSS Modal

--- EGrocer.UI/EGrocer/src/app/features/checkout/components/user-address/user-address.component.ts
+++ EGrocer.UI/EGrocer/src/app/features/checkout/components/user-address/user-address.component.ts
@@ -9,6 +9,10 @@ import { IUserAddress } from '../../models/user-address-i';
 })
 export class UserAddressComponent implements OnInit {
   userAddress!: IUserAddress;
+  userAddresses!: IUserAddress[];
+  selectedAddress!: IUserAddress;
+  isModalOpen: boolean = false;
+  isDefault: boolean = false;
 
   constructor(
     @Inject(UserAddressServiceToken)
@@ -20,14 +24,36 @@ export class UserAddressComponent implements OnInit {
   }
 
   changeAddress() {
+    this.selectedAddress = { ...this.userAddress }; // Clone the current user address
+    this.isModalOpen = true;
+  }
 
+  selectAddress(address: IUserAddress) {
+    this.selectedAddress = address;
+  }
+
+  saveAddress() {
+    this.userAddress = { ...this.selectedAddress }; // Update the user address with the selected address
+    this.isModalOpen = false;
+  }
+
+  cancelAddress() {
+    this.isModalOpen = false;
   }
 
   private loadAddress() {
     const userId = localStorage.getItem("userId");
     if (userId)
       this._userAddressService.get(userId).subscribe((response) => {
-        this.userAddress = response[0];
+        this.userAddresses = response;
+        const defaultAddress = this.userAddresses.filter((address) => address.isDefault);
+
+        // Check if a default address is found
+        if (defaultAddress.length > 0) {
+          this.userAddress = defaultAddress[0];
+        } else if (this.userAddresses.length > 0) {
+          this.userAddress = this.userAddresses[0];
+        }
       });
   }
 }
